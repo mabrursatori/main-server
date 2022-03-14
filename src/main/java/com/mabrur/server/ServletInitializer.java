39c9d8a027b40a6dddda0cb6b7101891b6566363
mabrursatori <mabrursatori@gmail.com>
2022-03-14 15:16:48 +0700

    Initial commit

---
+++ src/main/java/com/mabrur/server/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mabrur.server;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServerApplication.class); // Replace DemoApplication with your main class
+    }
+
+}
