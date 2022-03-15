f06f79d1b867a099be73aac77338b04415bd0de9
mabrursatori <mabrursatori@gmail.com>
2022-03-15 09:17:02 +0700

    Test DB

---
+++ src/main/java/com/mabrur/server/doa/entities/Category.java
@@ -0,0 +1,26 @@
+package com.mabrur.server.doa.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.Transient;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = false)
+    private String name;
+
+    @Transient
+    private Integer age;
+
+}
